0d61f971f54ba874f150b7219199b615e9f8e9fe
Oaklight <pding.dp@qq.com>
2018-06-21 11:56:56 +0800

    fix connection and namespace problem, add initiate function, add some comment, remove some useless session in jupyter notebook

--- gradeSender.py
+++ gradeSender.py
@@ -8,16 +8,18 @@ DEBUG = True
 user_account = ""
 user_password = "" # autherization code if using 163, qq, etc.
 user_smtp_server = ""
-user_smtp_port = 587
+user_smtp_port = ""
 file_to_open = "" # currently only support csv file parsing
 sent_list_file = ""
 waiting_list, sent_list = {}, {}
+subject = ""
 content = ""
+sleep = False
 
 def send(mail, name, id, score):
     msg = MIMEText(content.format(name, id, score), 'html')
     msg['From'] = user_account
-    msg['Subject'] = "Midterm Score of SI131 Linear Algebra"
+    msg['Subject'] = subject
     if DEBUG:
         msg['To'] = user_account
     else:
@@ -25,20 +27,30 @@ def send(mail, name, id, score):
     server.send_message(msg)
 
 def connect(user_smtp_server, user_smtp_port, user_account, user_password):
-    server = smtplib.SMTP(user_smtp_server, user_smtp_port)
+    head = user_smtp_server.split('.')
+    if head[0] == "mail":
+        server = smtplib.SMTP(user_smtp_server)
+    else:
+        if len(user_smtp_port) != 0:
+            user_smtp_port = eval(user_smtp_port)
+            server = smtplib.SMTP(user_smtp_server, user_smtp_port)
+            sleep = True
+        else:
+            user_smtp_server = user_smtp_server.replace("smtp", "mail", 1)
+            server = smtplib.SMTP(user_smtp_server)        
     reply = server.ehlo()
     if reply[0] == 250:
-        print('Conneted!', flush = True)
+        print('Conneted to {}!'.format(user_smtp_server), flush = True)
     else:
-        print('Failed to connect!', flush = True)
+        print('Failed to connect to {}!'.format(user_smtp_server), flush = True)
         exit(1)
     server.starttls()
     reply = server.login(user_account, user_password)
     if reply[0] == 235:
-        print('Logged in!', flush = True)
+        print('Logged in as {}!'.format(user_account), flush = True)
     else:
         server.quit()
-        print('Failed to login!', flush = True)
+        print('Failed to login as {}!'.format(user_account), flush = True)
         exit(1)
     return server
 
@@ -63,24 +75,36 @@ def write_sent_list(waiting_list, sent_list):
             record = (', '.join(waiting_list[i]))+ ', ' + str(sent_list[i]) + '\n'
             print(record)
             output.write(record)
-
+    
 def read_sent_list(file_to_open, sent_list):
     with open(file_to_open, newline='', encoding='utf-8') as verify:
         reader = verify.readlines()
         for i, row in enumerate(reader):
             record = [x for x in re.split('[,\s]+', row) if x != '']
-            sent_list[i] = eval(record[4].title())
+            sent_list[i] = eval(record[5].title())
         return sent_list
     
 def print_list(waiting_list, sent_list):
     for i in range(len(waiting_list)):
         id, name, email, score = waiting_list[i]
         print(name, email, score, sent_list[i])
-        # print(person)
-    
+
+def initiate(file_to_open = file_to_open, 
+             waiting_list = waiting_list, 
+             preview = False):
+    waiting_list, sent_list = read_waiting_list(file_to_open, waiting_list)
+    try:
+        sent_list = read_sent_list(sent_list_file, sent_list)
+    except:
+        write_sent_list(waiting_list, sent_list)
+    if preview:
+        print_list(gs.waiting_list, gs.sent_list)
+    return waiting_list, sent_list
+
 def send_score(waiting_list, sent_list):
     print('Total waiting: {}'.format(len(waiting_list)))
     for i in range(len(waiting_list)):
+        print(len(sent_list))
         if sent_list[i]:
             print("Already sent to {}".format((waiting_list[i])[1]))
             continue
@@ -107,12 +131,12 @@ def send_score(waiting_list, sent_list):
             else:
                 sent_list[i] = True
                 break
-    #     print('checkpoint2', flush=True)
-        if i % 5 == 4:
+        if sleep & (i % 5 == 4):
             print('Waiting for regular timeout ... ', end = '', flush = True)
             time.sleep(65)
             print('Done.', flush = True)
             server = connect(user_smtp_server, user_smtp_port, user_account, user_password)
+    print('')
     print('All sent.')        
     server.quit()
     
